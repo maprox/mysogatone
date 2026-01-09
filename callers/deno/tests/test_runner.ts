0630ebf1061b81c5a42468ad2293ce1a946c18d0
Александр Ляпко <lyapko@vbankcenter.ru>
2026-01-09 09:39:59 +0400

    chore: fix imports order for test files

--- callers/deno/tests/test_runner.ts
+++ callers/deno/tests/test_runner.ts
@@ -6,18 +6,20 @@
  */
 
 // Storage Provider тесты
-import "./utils_test.ts";
 import "./errors_test.ts";
 import "./file-operations_test.ts";
 import "./http-client_test.ts";
-import "./yandex-disk-provider_test.ts";
 
 // SOCKS5 тесты
 import "./socks5-handler.test.ts";
 import "./socks5-server.test.ts";
 
+// Utilities
+import "./utils_test.ts";
+
 // YandexDiskConnectionHandler тесты
 import "./yandex-disk-connection-handler.test.ts";
+import "./yandex-disk-provider_test.ts";
 
 // Интеграционные тесты
 // Integration tests removed - will be added separately
